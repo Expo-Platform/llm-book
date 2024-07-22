4b4680a5e0e5bc7697a21e500271af338b2e3443
Mahesh Rajput <m.rajput@expoplatform.com>
2024-07-22 17:52:17 +0530

    [] Init readthedocs

---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The Hitchhiker’s Guide to LLMs for Events: From Zero to Production'
+copyright = '2024, ExpoPlatform'
+author = 'ExpoPlatform'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. The Hitchhiker’s Guide to LLMs for Events: From Zero to Production documentation master file, created by
+   sphinx-quickstart on Mon Jul 22 17:48:58 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to The Hitchhiker’s Guide to LLMs for Events: From Zero to Production's documentation!
+==============================================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
